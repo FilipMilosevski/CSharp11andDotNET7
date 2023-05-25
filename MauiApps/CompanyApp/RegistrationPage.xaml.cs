1dbe6f2129dcfffc8fb0d3578c44cae1a8806014
FilipMilosevski <filipdh94@gmail.com>
2023-05-25 20:30:08 +0200

    company app 25.05

---
+++ MauiApps/CompanyApp/RegistrationPage.xaml.cs
@@ -0,0 +1,15 @@
+namespace CompanyApp;
+
+public partial class RegistrationPage : ContentPage
+{
+	public RegistrationPage()
+	{
+		InitializeComponent();
+	}
+
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+		await DisplayAlert("Alert", "Succesfull registration", "OK");
+		await Navigation.PopAsync();
+    }
+}
