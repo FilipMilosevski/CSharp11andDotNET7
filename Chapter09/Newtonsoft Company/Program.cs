a7aab8accb99a0379783096613db00c9ae72c16f
FilipMilosevski <filipdh94@gmail.com>
2023-05-27 00:35:52 +0200

    serialize 27.05

--- Chapter09/Newtonsoft Company/Program.cs
+++ Chapter09/Newtonsoft Company/Program.cs
@@ -197,10 +197,65 @@ List<Company> companies = new List<Company>()
 
 };
 
+//string json = JsonConvert.SerializeObject(companies, Formatting.Indented);
+//WriteLine(json);
+
+//File.WriteAllText(@"C:\CSharp11andDotNET7\Chapter09\Newtonsoft Company\bin\Debug\net7.0\companies.json", json);
+//using (StreamWriter file = File.CreateText(@"C:\CSharp11andDotNET7\Chapter09\Newtonsoft Company\bin\Debug\net7.0\companies.json"))
+//{
+//    JsonSerializer jss = new JsonSerializer();
+//    jss.Serialize(file, companies);
+//}
+
+//List<Company> companyList = JsonConvert.DeserializeObject<List<Company>>(json);
+
+//SectionTitle("IMINJA NA CEO");
+
+//if (companyList != null)
+//{
+//    foreach (Company com in companyList)
+//    {
+//        WriteLine(com.firstName + " " + com.lastName + " " + com.position);
+//    }
+//}
+
+//SectionTitle("KOLKU VRABOTENI IMAAT POD NIV");
+
+//if (companyList != null)
+//{
+//    foreach (Company com in companyList)
+//    {
+//        int employeeCount = com.klas != null ? com.klas.Count : 0;
+//        WriteLine($"{com.firstName} has {employeeCount} people as assosiates");
+//    }
+//}
+
+//SectionTitle("BROJ I IMINJA NA VRABOTENITE");
+
+//if (companyList != null)
+//{
+//    foreach (Company com in companyList)
+//    {
+//        int employeeCount = com.klas != null ? com.klas.Count : 0;
+//        WriteLine($"{com.firstName} has {employeeCount} people as assosiates");
+//        if (com.klas != null)
+//        {
+//            foreach (Company emp in com.klas)
+//            {
+//                WriteLine($"{com.firstName.PadLeft(30)} is the {com.position}'S on {emp.firstName}");
+//            }
+//        }
+
+//    }
+
+
+//}
+
 string json = JsonConvert.SerializeObject(companies, Formatting.Indented);
 WriteLine(json);
 
 File.WriteAllText(@"C:\CSharp11andDotNET7\Chapter09\Newtonsoft Company\bin\Debug\net7.0\companies.json", json);
+
 using (StreamWriter file = File.CreateText(@"C:\CSharp11andDotNET7\Chapter09\Newtonsoft Company\bin\Debug\net7.0\companies.json"))
 {
     JsonSerializer jss = new JsonSerializer();
@@ -211,13 +266,14 @@ List<Company> companyList = JsonConvert.DeserializeObject<List<Company>>(json);
 
 SectionTitle("IMINJA NA CEO");
 
-if(companyList !=null)
+if (companyList != null)
 {
     foreach (Company com in companyList)
     {
-        WriteLine(com.firstName+" "+com.lastName+" "+com.position);
+        WriteLine($"{com.firstName} is the {com.position} of {com.companyName}");
     }
 }
+
 
 SectionTitle("KOLKU VRABOTENI IMAAT POD NIV");
 
@@ -225,36 +281,32 @@ if (companyList != null)
 {
     foreach (Company com in companyList)
     {
-        int employeeCount = com.klas != null ? com.klas.Count : 0;
-        WriteLine($"{com.firstName} has {employeeCount} people as assosiates");
+        int kidCount = com.klas != null ? com.klas.Count : 0;
+        WriteLine($"{com.firstName} has {kidCount} as assosiates.");
     }
 }
 
+
 SectionTitle("BROJ I IMINJA NA VRABOTENITE");
+
 
 if (companyList != null)
 {
     foreach (Company com in companyList)
     {
-        int employeeCount = com.klas != null ? com.klas.Count : 0;
-        WriteLine($"{com.firstName} has {employeeCount} people as assosiates");
-        if(com.klas != null)
+        int kidcount = com.klas != null ? com.klas.Count : 0;
+        WriteLine($"{com.firstName} has {kidcount} as assosiates.");
+        if (com.klas != null)
         {
             foreach (Company emp in com.klas)
             {
-                WriteLine($"{com.firstName.PadLeft(30)} are the {com.position}'S on {emp.firstName}");
+                //WriteLine($"{emp.firstName.PadLeft(30)} ");
+                WriteLine($"{com.firstName.PadLeft(30)} is the {com.position}'S on {emp.firstName}");
+
             }
         }
 
+
+
     }
-
-
-}
-
-SectionTitle("FIlip");
-SectionTitle("fico");
-
-
-WriteLine("Proba");
-WriteLine("READ");
-
+};
