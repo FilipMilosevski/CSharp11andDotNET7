2a25f00c686323f4aaba7cce4fe3667251cb7477
FilipMilosevski <filipdh94@gmail.com>
2023-05-27 10:46:47 +0200

    27.05 quizz

---
+++ Quizz/QuizzHome/Program.cs
@@ -0,0 +1,86 @@
+﻿using QuizzHome;
+
+Console.WriteLine("Which quizz do you want to play: Animal or Car?");
+
+
+string quizSelection = Console.ReadLine().ToLower().Trim();
+
+if (quizSelection == "animal")
+{
+    QuizzQuestion1.setAnimalQuestion();
+}
+else if (quizSelection == "car")
+{
+    QuizzQuestion1.setCarQuestion();
+}
+else
+{
+    Console.WriteLine("Thanks for playing!");
+    return;
+}
+
+
+
+foreach (Question1 question in QuizzQuestion1.questions)
+{
+    Console.WriteLine(question.Text);
+    for (int i = 0; i < question.Options.Length; i++)
+    {
+        Console.WriteLine($"{i + 1} . {question.Options[i]}");
+    }
+    string userAnswer = Console.ReadLine().Trim();
+    if (userAnswer.ToLower() == question.Answer.ToLower())
+    {
+        Console.WriteLine();
+        Console.WriteLine("You guessed right!");
+        Console.WriteLine();
+    }
+    else
+    {
+        Console.WriteLine();
+        Console.WriteLine($"The right answer was {question.Answer}");
+        Console.WriteLine();
+
+    };
+}
+Console.WriteLine();
+Console.WriteLine();
+
+Console.WriteLine("Do you want to continue with the quiz?");
+string continueResponse = Console.ReadLine().ToLower().Trim();
+if (continueResponse == "yes")
+{
+    Console.WriteLine("Let's continue!");
+}
+else
+{
+    Console.WriteLine("Thanks for playing! Goodbye.");
+    return;
+}
+
+QuizzQuestion1.setCarQuestion();
+
+
+foreach (Question1 question in QuizzQuestion1.questions)
+{
+    Console.WriteLine(question.Text);
+    for (int i = 0; i < question.Options.Length; i++)
+    {
+        Console.WriteLine($"{i + 1} . {question.Options[i]}");
+    }
+    string userAnswer = Console.ReadLine().Trim();
+    if (userAnswer.ToLower() == question.Answer.ToLower())
+    {
+        Console.WriteLine();
+        Console.WriteLine("You guessed right!");
+        Console.WriteLine();
+    }
+    else
+    {
+        Console.WriteLine();
+        Console.WriteLine($"The right answer was {question.Answer}");
+        Console.WriteLine();
+
+    };
+}
+
---
+++ Quizz/QuizzHome/QuizzQuestion1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizzHome
+{
+    public static class QuizzQuestion1
+    {
+        public static Question1[] questions { get;private set; }    
+        public static void setAnimalQuestion()
+        {
+            questions = new Question1[]
+            {
+                new Question1("What is dog?","two", new string[]{"one","two","three"}),
+                new Question1("What is cat", "one",null),
+                new Question1("What is horse","three",null)
+            };
+        }
+        public static void setCarQuestion()
+        {
+            questions = new Question1[]
+            {
+                new Question1("Mondeo is model of ?","ford",new string[]{"vw","seat","ford"}),
+                new Question1("Passat is model of ?","vw",null),
+                new Question1("Ibiza is model of ?", "seat",null),
+                new Question1("Jimmy is model of ?", "suzuki", new string[]{"suzuki","audi","bmw"})
+            };
+        }
+
+    }
+}
