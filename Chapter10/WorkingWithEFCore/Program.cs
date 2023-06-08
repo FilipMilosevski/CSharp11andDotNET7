215bf4c6667dad6c1e3af5134e379ad896f7236c
FilipMilosevski <filipdh94@gmail.com>
2023-06-08 21:08:59 +0200

    08 06 north wind

--- Chapter10/WorkingWithEFCore/Program.cs
+++ Chapter10/WorkingWithEFCore/Program.cs
@@ -4,11 +4,42 @@
 Northwind db = new Northwind();
 WriteLine($"Provider: {db.Database.ProviderName}");
 
-SectionTitle("filip");
-SectionTitle("proba");
-Fail("fi");
-Info("di");
 
-QueryingCategories();
 
-QueryingProducts();
+//QueryingCategories();
+
+//QueryingProducts();
+
+
+
+//var resultAdd = AddProduct(6, "Bob's burgrers", 500M);
+
+//if(resultAdd.affected == 1) 
+//{
+//    WriteLine($"Added product successfully with ID: {resultAdd.productId}");
+//}
+
+//ListProducts(new int[] { resultAdd.productId });
+
+
+
+
+//var resultAdd1 = IncreaseProductPrice("Bob", 20M);
+
+//if (resultAdd1.affected == 1)
+//{
+//    WriteLine($"Added CHANGE product successfully with ID: {resultAdd1.productId}");
+//}
+
+//ListProducts(new int[] { resultAdd1.productId });
+
+
+
+var delres = DeleteProduct("Bob");
+
+if(delres == 1)
+{
+    WriteLine($"Deleted product successfully with that starts with Bob");
+}
+
+ListProducts();
