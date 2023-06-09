2f732d883fb369cb04cd4e743a69e9a4051f9862
FilipMilosevski <filipdh94@gmail.com>
2023-06-09 23:40:40 +0200

    09 06 sql

--- Chapter10/WorkingWithEFCore/Program.cs
+++ Chapter10/WorkingWithEFCore/Program.cs
@@ -12,34 +12,38 @@ WriteLine($"Provider: {db.Database.ProviderName}");
 
 
 
-//var resultAdd = AddProduct(6, "Bob's burgrers", 500M);
+var resultAdd = AddProduct(6, "Bob's burgrers", 500M);
 
-//if(resultAdd.affected == 1) 
-//{
-//    WriteLine($"Added product successfully with ID: {resultAdd.productId}");
-//}
-
-//ListProducts(new int[] { resultAdd.productId });
-
-
-
-
-//var resultAdd1 = IncreaseProductPrice("Bob", 20M);
-
-//if (resultAdd1.affected == 1)
-//{
-//    WriteLine($"Added CHANGE product successfully with ID: {resultAdd1.productId}");
-//}
-
-//ListProducts(new int[] { resultAdd1.productId });
-
-
-
-var delres = DeleteProduct("Bob");
-
-if(delres == 1)
+if (resultAdd.affected == 1)
 {
-    WriteLine($"Deleted product successfully with that starts with Bob");
+    WriteLine($"Added product successfully with ID: {resultAdd.productId}");
 }
 
-ListProducts();
+ListProducts(new int[] { resultAdd.productId });
+
+
+
+
+var resultAdd1 = IncreaseProductPrice("Bob", 20M);
+
+if (resultAdd1.affected == 1)
+{
+    WriteLine($"Added CHANGE product successfully with ID: {resultAdd1.productId}");
+}
+
+ListProducts(new int[] { resultAdd1.productId });
+
+
+
+//var delres = DeleteProduct("Bob");
+
+//if(delres == 1)
+//{
+//    WriteLine($"Deleted product successfully with that starts with Bob");
+//}
+
+//ListProducts();
+
+
+WriteLine("filip");
+WriteLine("milosevski");
