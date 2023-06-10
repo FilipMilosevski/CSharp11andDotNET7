20b256f72bc8d631892b4d5a0a36c7316330f9dd
FilipMilosevski <filipdh94@gmail.com>
2023-06-10 18:21:48 +0200

    10.06 practicing sql

---
+++ Chapter10/PracticeSaturdayEntity/EFContext_cs.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PracticeSaturdayEntity
+{
+    public class EFContext_cs : DbContext
+    {
+        private const string connectionString 
+    }
+}
