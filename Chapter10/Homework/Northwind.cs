c9109698b5eeb725bd26e0252f3a481aaf907c6f
FilipMilosevski <filipdh94@gmail.com>
2023-06-12 06:40:14 +0200

    monday practice

--- Chapter10/Homework/Northwind.cs
+++ Chapter10/Homework/Northwind.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,10 +10,14 @@ namespace Packt.Shared;
 
 public class Northwind : DbContext
 {
-   
+
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Product> Products { get; set; }    
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        string path = Path.Combine(Environment.CurrentDirectory, "Northwind.db");
+
+        string path = Path.Combine(Environment.CurrentDirectory,"sql-scripts", "Northwind.db");
         string connection = $"FileName = {path}";
 
         ConsoleColor previousColor = ForegroundColor;
