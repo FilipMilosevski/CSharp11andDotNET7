d30728eb18c0cd954b375b69670eb7a9b25ad3d7
FilipMilosevski <filipdh94@gmail.com>
2023-06-14 21:48:30 +0200

    14 06 console app

---
+++ Chapter10/PracticeLoginProjectApp/LoginOrCreateUser.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PracticeLoginProjectApp
+{
+    internal static class LoginOrCreateUser
+    {
+        private static string _username;
+        private static string _password;
+
+        public static void Login()
+        {
+
+        }
+        public static void Create()
+        {
+
+        }
+        #region Helpers
+        private static bool isPasswordAllowed(string? password)
+        {
+            Regex regx = new Regex(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,15}$");
+            if (regx.IsMatch(password) && !String.IsNullOrEmpty(password))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private static bool isUsernameAllowed(string? username)
+        {
+            Regex regx = new Regex(@"^(?=[A-Za-z0-9])(?!.*[._()\[\]-]{2})[A-Za-z0-9._()\[\]-]{3,15}$");
+
+            if (regx.IsMatch(username) && !String.IsNullOrEmpty(username))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        #endregion
+    }
+}
---
+++ Chapter10/PracticeLoginProjectApp/Program.cs
@@ -0,0 +1,62 @@
+﻿
+//create
+using System.Text.RegularExpressions;
+
+//Console.WriteLine("Enter username");
+//string createUsername = Console.ReadLine();
+
+//Console.WriteLine("Enter password");
+//string createPassword = Console.ReadLine();
+
+//Console.WriteLine("Confirm password");
+//string confirmPassword = Console.ReadLine();
+
+
+bool isLoginCorrect = false;
+do
+{
+    //login
+    Console.WriteLine("Enter username");
+    string username = Console.ReadLine();
+
+    Console.WriteLine("Enter password");
+    string password = Console.ReadLine();
+
+    if (isUsernameAllowed(username) && isPasswordAllowed(password))
+    {
+        isLoginCorrect = true;
+    }
+    else
+    {
+        if (isUsernameAllowed(username))
+        {
+            Console.WriteLine("Username is incorrect");
+        }
+        if (isPasswordAllowed(password))
+        {
+            Console.WriteLine("Password is incorrect");
+        }
+    }
+        
+} while (!isLoginCorrect);
+
+bool isPasswordAllowed(string? password)
+{
+    Regex regx = new Regex(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,15}$");
+    if (regx.IsMatch(password) && !String.IsNullOrEmpty(password))
+    {
+        return true;
+    }
+    return false;
+}
+
+bool isUsernameAllowed(string? username)
+{
+    Regex regx = new Regex(@"^(?=[A-Za-z0-9])(?!.*[._()\[\]-]{2})[A-Za-z0-9._()\[\]-]{3,15}$");
+
+    if (regx.IsMatch(username) && !String.IsNullOrEmpty(username))
+    {
+        return true;
+    }
+    return false;
+}
