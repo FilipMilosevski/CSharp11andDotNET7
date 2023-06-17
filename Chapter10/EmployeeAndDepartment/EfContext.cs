02b230872cdab97bc570572540662b70c4e314ba
FilipMilosevski <filipdh94@gmail.com>
2023-06-17 20:51:45 +0200

    practice sat 17.06

---
+++ Chapter10/EmployeeAndDepartment/EfContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+
+namespace EmployeeAndDepartment
+{
+    public class EfContext : DbContext
+    {
+        private const string connectionString = "Server=Filip;Database=EFCore12Trusted_Connection=SSPI;Encrypt=false;TrustServerCertificate=true";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { optionsBuilder.UseSqlServer(connectionString); }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+
+    }
+}
